86661f2ac33bdce53f124cdc05fa4987103efb53
Casey Brock <caseybrock@vanderbilt.edu>
2017-04-06 10:19:08 -0500

    updated test script to use new word lists

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import mycount
 
 formatted_harry_txt = mycount.format_harry_potter('harry_potter_stein_weisen.txt')
-kw = mycount.read_known_word_list('duolingo_words_learned.txt')
-b = mycount.Book(formatted_harry_txt, kw)
+kw_duolingo = mycount.read_known_word_list('duolingo_words_learned.txt')
+kw_2000 = mycount.read_known_word_list('german_vocab_2000.txt')
+kw_characters = mycount.read_known_word_list('character_names.txt')
+all_known_words = list(set(kw_duolingo) | set(kw_2000) | set(kw_characters))
+b = mycount.Book(formatted_harry_txt, all_known_words)
