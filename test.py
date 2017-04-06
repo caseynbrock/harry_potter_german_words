1d9d678f74b94a66102f99c4f6e38410e06818b0
Casey Brock <caseybrock@vanderbilt.edu>
2017-04-06 16:39:44 -0500

    moved all known word lists to directory

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import mycount
 
 formatted_harry_txt = mycount.format_harry_potter('harry_potter_stein_weisen.txt')
-kw_duolingo = mycount.read_known_word_list('duolingo_words_learned.txt')
-kw_2000 = mycount.read_known_word_list('german_vocab_2000.txt')
-kw_characters = mycount.read_known_word_list('character_names.txt')
+kw_duolingo = mycount.read_known_word_list('known_word_files/duolingo_words_learned.txt')
+kw_2000 = mycount.read_known_word_list('known_word_files/german_vocab_2000.txt')
+kw_characters = mycount.read_known_word_list('known_word_files/character_names.txt')
 all_known_words = list(set(kw_duolingo) | set(kw_2000) | set(kw_characters))
 b = mycount.Book(formatted_harry_txt, all_known_words)
